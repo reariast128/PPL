1a9dfb3cb223cd077e7e041156e2ff2c5f7a185b
Ricardo Arias <reariast128@proton.me>
2024-02-23 22:08:22 -0500

    First tokens definitions.

---
+++ tkns.py
@@ -0,0 +1,26 @@
+from enum import auto, Enum, unique
+from typing import NamedTuple
+
+@unique
+class TokenType(Enum):
+    ASSIGN = auto()
+    COMMA = auto()
+    EOF = auto()
+    FUNCTION = auto()
+    IDENT = auto()
+    ILLEGAL = auto()
+    INT = auto()
+    LBRACE = auto()
+    LET = auto()
+    LPARENT = auto()
+    PLUS = auto()
+    RBRACE = auto()
+    RPARENT = auto()
+    SEMICOLON = auto()
+
+class Token(NamedTuple):
+    token_type: TokenType
+    literal: str
+
+    def __str__(self) -> str:
+        return f"Type: {self.token_type}, Literal: {self.literal}"
